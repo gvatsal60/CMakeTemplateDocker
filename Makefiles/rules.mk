b8bd3195adcfdc8d4658755bcb7ecc3ab3031d6c
Vatsal Gupta <gvatsal60>
2024-12-17 19:02:47 +0530

    New Fixes

--- Makefiles/rules.mk
+++ Makefiles/rules.mk
@@ -6,12 +6,12 @@ include cfg/.env
 # Commands
 CMAKE := cmake
 MKDIR := mkdir -p
-RMDIR := rm -r
+RMDIR := rm -rf
 
 # Build Commands
-BUILD_TOOL_CMD := $(MKDIR) $(BUILD_DIR); $(CMAKE) --build $(BUILD_DIR) --parallel $(shell nproc)
+BUILD_TOOL_CMD := $(CMAKE) -S $(TOP_DIR) -B $(BUILD_DIR) && $(CMAKE) --build $(BUILD_DIR) --parallel $(shell nproc)
 BUILD_CMD := $(BUILD_TOOL_CMD)
-TEST_CMD := $(BUILD_TOOL_CMD) && ./$(TEST_DIR)/$(TEST_EXEC)
-RUN_CMD := $(BUILD_TOOL_CMD) && ./$(BUILD_DIR)/$(DEBUG_EXEC)
-CLEAN_CMD := $(BUILD_TOOL_CMD) && $(RMDIR) $(BUILD_DIR)/**
+TEST_CMD := ./$(TEST_DIR)/$(TEST_EXEC)
+RUN_CMD := ./$(BUILD_DIR)/$(DEBUG_EXEC)
+CLEAN_CMD := $(RMDIR) $(BUILD_DIR)/**
 # SONAR_CMD := $(BUILD_TOOL_CMD) sonar
