d42a86a22e1874b2ed3e68b2ba685003e84180fe
Vatsal Gupta <gvatsal60>
2024-12-17 17:40:32 +0530

    Make Fixes

--- Makefiles/rules.mk
+++ Makefiles/rules.mk
@@ -9,7 +9,7 @@ MKDIR := mkdir -p
 RMDIR := rm -r
 
 # Build Commands
-BUILD_TOOL_CMD := $(MKDIR)/$(BUILD_DIR); $(CMAKE) --build $(BUILD_DIR) --parallel $(shell nproc)
+BUILD_TOOL_CMD := $(MKDIR) $(BUILD_DIR); $(CMAKE) --build $(BUILD_DIR) --parallel $(shell nproc)
 BUILD_CMD := $(BUILD_TOOL_CMD)
 TEST_CMD := $(BUILD_TOOL_CMD) && ./$(TEST_DIR)/$(TEST_EXEC)
 RUN_CMD := $(BUILD_TOOL_CMD) && ./$(BUILD_DIR)/$(DEBUG_EXEC)
